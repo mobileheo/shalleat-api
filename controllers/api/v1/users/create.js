03f1be263be80e8ae4446d543cb0ce37ba891a86
Sunny Heo <mobileheo@gmail.com>
2018-06-06 00:33:10 -0700

    Add password match condition on api/v1/users/create

--- controllers/api/v1/users/create.js
+++ controllers/api/v1/users/create.js
@@ -18,19 +18,51 @@ module.exports = {
       pwMatch
     } = req.body;
     const provider = "local"
-    bcrypt.hash(password, saltRounds, async function (err, password) {
-      try {
-        const user = await User.query().insert({
-          username, email, firstName, lastName, password, provider
-        })
-        req.login(user.id, err => {
-          console.log(`err => ${err}`)
-          res.json({username, email, firstName, lastName, provider})
-        });
-      } catch (err) {
-          console.log(err.message);
-          res.json({error: err.message});
+    module.exports = {
+      async localLogin(req, res, next) {
+        const {
+          username,
+          email,
+          firstName,
+          lastName,
+          password,
+          pwMatch
+        } = req.body;
+        const provider = "local"
+        if (password !== pwMatch) {
+          res.json({
+            error: "Passwords do not match, please try again."
+          })
+        } else {
+          bcrypt.hash(password, saltRounds, async function (err, password) {
+            try {
+              const user = await User.query().insert({
+                username,
+                email,
+                firstName,
+                lastName,
+                password,
+                provider
+              })
+              req.login(user.id, err => {
+                console.log(`err => ${err}`)
+                res.json({
+                  username,
+                  email,
+                  firstName,
+                  lastName,
+                  provider
+                })
+              });
+            } catch (err) {
+              console.log(err.message);
+              res.json({
+                error: err.message
+              });
+            }
+          });
+        }
       }
-    });
+    }
   }
 }
