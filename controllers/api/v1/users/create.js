f47ba10cbe3bd8101b86cb6f7fa8dda4b4fd3923
Sunny Heo <mobileheo@gmail.com>
2018-06-05 22:14:52 -0700

    Add response when receiving an invalid user json and property 'provider

--- controllers/api/v1/users/create.js
+++ controllers/api/v1/users/create.js
@@ -2,11 +2,10 @@ const knex = require("../../../../db"),
   passport = require("passport"),
   session = require("express-session"),
   bcrypt = require("bcrypt"),
+  saltRounds = 10,
   {
     User,
-    Restaurant
-  } = require('../../../../models/schema'),
-  saltRounds = 10;
+  } = require('../../../../models/schema');
 
 module.exports = {
    async localLogin(req, res, next) {
@@ -18,20 +17,20 @@ module.exports = {
       password,
       pwMatch
     } = req.body;
-
+    const provider = "local"
     bcrypt.hash(password, saltRounds, async function (err, password) {
       try {
         const user = await User.query().insert({
-          username, email, first_name, last_name, password
+          username, email, first_name, last_name, password, provider
         })
         req.login(user.id, err => {
           console.log(`err => ${err}`)
-          res.json(user)
+          res.json({username, email, firstanme, last_name, provider})
         });
       } catch (err) {
         console.log(err instanceof objection.ValidationError); // --> true
         console.log(err.data); // --> {lastName: [{message: 'required property missing', ...}]}
-        next();
+        res.json(err.data);
       }
     });
   }
