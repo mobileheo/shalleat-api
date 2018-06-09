fbba5ac42284cac9d2667b918ac03f06332b9354
Sunny Heo <mobileheo@gmail.com>
2018-06-08 23:37:07 -0700

    Fix syntax errors on reqValidations and Users#signUp

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,9 +1,11 @@
-const express = require("express");
+// const express = require("express");
 const router = require("express-promise-router")();
-
+const { validateUser, schemas } = require("../../reqValidations");
 const UsersController = require("../../../controllers/api/v1/users/users");
 
-router.route("/signup").post(UsersController.signUp);
+router
+  .route("/signup")
+  .post(validateUser(schemas.authSchema), UsersController.signUp);
 router.route("/signin").post(UsersController.signIn);
 router.route("/secret").get(UsersController.secret);
 
--- routes/reqValidations.js
+++ routes/reqValidations.js
@@ -1,30 +1,29 @@
-const joi = requrie("joi");
+const Joi = require("joi");
 const VALID_PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/i;
 
 module.exports = {
-  validateUser: schema => {
-    const result = joi.validate(req.body, schema);
-    const { password, pwMatch } = req.body;
+  validateUser: schema => (req, res, next) => {
+    const result = Joi.validate(req.body, schema);
+    const { pwMatch, ...user } = req.body;
     if (result.error) return res.status(400).json(result.error);
 
-    if (password !== pwMatch)
+    if (user.password !== pwMatch)
       return res.status(400).json({
         error: "Passwords do not match, please try again."
       });
 
     if (!req.value) req.value = {};
-    const { pwMatch, ...user } = req.body;
     req.value.body = user;
     next();
   },
 
-  schema: {
+  schemas: {
     authSchema: Joi.object().keys({
       email: Joi.string()
         .email()
         .required(),
-      firstName: Joi.string.requrired(),
-      lastName: Joi.string.requrired(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
       password: Joi.string()
         .regex(VALID_PASSWORD_REGEX)
         .required(),
