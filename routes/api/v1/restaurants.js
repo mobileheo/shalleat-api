ca7845850b0e95f53237b41acff0e5b1dc90f795
Sunny Heo <mobileheo@gmail.com>
2018-06-24 17:23:31 -0700

    Google place pai (#20)
    * Create routes for restaurants api & Integrate google place api with express routes
    
    * Add routes to fetch place detail with given placeId and filters and send it to the react client
    
    * Add functions to parse place detail json to provide today and nextDay business availability and hours

---
+++ routes/api/v1/restaurants.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+const router = require("express-promise-router")();
+const passport = require("passport");
+const passportConfig = require("../../../passport");
+
+const {
+  findAllRestaurants,
+  getRestaurantSchedule
+} = require("../../../controllers/api/v1/restaurants/restaurants");
+
+// const passportSignIn = passport.authenticate("local", { session: false });
+const passportJWT = passport.authenticate("jwt", { session: false });
+
+router.route("/all").post(passportJWT, findAllRestaurants);
+router.route("/detail").post(passportJWT, getRestaurantSchedule);
+
+// router
+//   .route("/signin")
+//   .post(validateSignIn(signInUserSchemas.authSchema), passportSignIn, signIn);
+
+// router.route("/secret").get(passportJWT, secret);
+
+module.exports = router;
